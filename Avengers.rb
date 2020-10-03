cdfbc63cf9a472be55a67e32a8846d26f73cc2d6
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2020-10-03 16:37:29 +0100

    Create Avengers.rb

---
+++ Avengers.rb
@@ -0,0 +1,150 @@
+#nane : Avengers Infinity War
+#Created by : CryptoLover705
+#Bugs : Unknown
+#Check Coments for Challenge Link...
+#Input Instructions : Enter the name of the Super Hero from the given hash below👇
+
+Avengers = Hash["iron man"=>1, "thor"=>2, "hulk"=>3, "captain america"=>4, "black widow"=>5, "doctor strange"=>6, "spider man"=>7, "black panther"=>8, "vision"=>9, "scarlet witch"=>10, "loki"=>11, "groot"=>12, "thanos"=>13]
+a = gets.chomp.downcase
+b = Avengers[a]
+puts "Avengers are ready for 'Thanos'\nAre U ready..."
+case b
+when 1
+  puts "                                                    
+________                     
+\\      /
+ \\    /               
+  \\  /
+   \\/
+   
+    Name : Tony Stark
+    
+    Aka : Iron Man
+    
+    Info :
+          'He is a genius, billionaire, playboy and philanthropist with a self-made electromechanical suits of armor'.                                          
+"
+when 2
+  puts "         
+ ______
+|      |         
+|______|
+   ||                     
+   ||                      
+   ||  
+                                     
+    Name : Thor
+    
+    Info : The king of Asgard based on the Norse mythology.
+    
+    Super power: 
+          'Thor now wields a mystical maul known as Stormbreaker, after the destruction of his hammer Mjolnir in Ragnarok'.                                                              
+  "
+when 3
+  puts "            
+| |   | |
+|/\/\/\/|                       
+|/\/\/\/|
+| |   | |
+                 
+    Name : Bruce Banner
+    
+    Aka : Hulk 
+    
+    Info : A genius scientist who, because of exposure to gamma radiation, transforms into a monster when enraged or agitated.                                 
+  "
+when 4
+  puts "
+   *
+*******
+  ***
+ *   *
+    Name : Steve Rogers
+    
+    Aka : Captain America
+    
+    Info : A super human physicality by an experimental serum 
+    
+    'He carries a shield made of Vibranium'
+  "
+when 5
+  puts "
+    Name : Natasha Romanoff
+    
+    Aka : Black Widow
+    
+    Info: A highly trained spy, former Avenger and an agent of S.H.I.E.L.D.
+    
+    Also she is hot :) XD
+  "
+when 6
+  puts "
+    Name : Stephen Strange
+    
+    Aka : Dr. Strange
+    
+    Info : A former neurosurgeon who, after a car accident that led to a journey of healing, discovered the hidden world of magic and alternate dimensions and became a Master of the Mystic Arts.
+  
+  "
+when 7
+  puts "
+    Name : Peter Parker
+    
+    Aka : Spider Man
+    
+    Info : A teenager and Stark's protege who received spider-like abilities after being bitten by a genetically-modified spider.
+  "
+#Created by : CryptoLover705 
+when 8
+  puts "
+    Name : T'Challa
+    
+    Aka : Black Panther
+    
+    Info : The king of the African nation of Wakanda, who gained his enhanced strength by ingesting the Heart-Shaped Herb.  
+  
+  "
+when 9
+  puts "
+    Name : Vision
+    
+    Info : An android and Avenger created using the artificial intelligence J.A.R.V.I.S. and the Mind Stone.
+    "
+when 10
+  puts "
+    Name : Wanda Maximoff
+    
+    Aka : Scarlet Witch
+    
+    Info : A member of Rogers' faction of Avengers, who can harness magic and engage in hypnosis and telekinesis.
+  
+  "
+when 11
+  puts "
+    Name : Loki
+    
+    Info : Thor's adoptive brother based on the Norse mythological deity of the same name.
+  
+  "
+when 12
+  puts "
+    Name : Groot
+    
+    Info : A member of the Guardians who is a tree-like humanoid.
+  
+  "
+when 13
+  puts "
+    Name : Thanos
+    
+    Aka : Main villain of \"Avengers: Infinity War\".
+    
+    Info : An intergalactic despot from Titan who longs to collect all of the Infinity Stones in order to impose his will on all of reality, wanting to \"re-balance the universe\".
+  
+  "
+else
+  puts "\nCheck Input Instructions properly\n\n"
+end
+
+puts "Run again to input different Super Hero\n\nHit the fork if you liked it.."
+#Created by : CryptoLover705 
